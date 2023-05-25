cdc69eb2e7f790039d69eb9c334b3de1c24831fe
rickyjooooooe <128315245+rickyjooooooe@users.noreply.github.com>
2023-05-25 09:34:01 +0700

    Add files via upload

---
+++ Saldo/Saldo/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Saldo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1000 = new System.Windows.Forms.Button();
+            this.button500 = new System.Windows.Forms.Button();
+            this.button200 = new System.Windows.Forms.Button();
+            this.button150 = new System.Windows.Forms.Button();
+            this.button100 = new System.Windows.Forms.Button();
+            this.button50 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-Money balance :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(496, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rangga Rijasa";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Top-up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1000);
+            this.panel1.Controls.Add(this.button20);
+            this.panel1.Controls.Add(this.button500);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button200);
+            this.panel1.Controls.Add(this.button50);
+            this.panel1.Controls.Add(this.button150);
+            this.panel1.Controls.Add(this.button100);
+            this.panel1.Location = new System.Drawing.Point(12, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 307);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1000
+            // 
+            this.button1000.Location = new System.Drawing.Point(597, 163);
+            this.button1000.Name = "button1000";
+            this.button1000.Size = new System.Drawing.Size(164, 90);
+            this.button1000.TabIndex = 15;
+            this.button1000.Text = "1.000.000";
+            this.button1000.UseVisualStyleBackColor = true;
+            // 
+            // button500
+            // 
+            this.button500.Location = new System.Drawing.Point(406, 163);
+            this.button500.Name = "button500";
+            this.button500.Size = new System.Drawing.Size(164, 90);
+            this.button500.TabIndex = 14;
+            this.button500.Text = "500.000";
+            this.button500.UseVisualStyleBackColor = true;
+            // 
+            // button200
+            // 
+            this.button200.Location = new System.Drawing.Point(212, 163);
+            this.button200.Name = "button200";
+            this.button200.Size = new System.Drawing.Size(164, 90);
+            this.button200.TabIndex = 13;
+            this.button200.Text = "200.000";
+            this.button200.UseVisualStyleBackColor = true;
+            // 
+            // button150
+            // 
+            this.button150.Location = new System.Drawing.Point(18, 163);
+            this.button150.Name = "button150";
+            this.button150.Size = new System.Drawing.Size(164, 90);
+            this.button150.TabIndex = 12;
+            this.button150.Text = "150.000";
+            this.button150.UseVisualStyleBackColor = true;
+            // 
+            // button100
+            // 
+            this.button100.Location = new System.Drawing.Point(597, 43);
+            this.button100.Name = "button100";
+            this.button100.Size = new System.Drawing.Size(164, 90);
+            this.button100.TabIndex = 11;
+            this.button100.Text = "100.000";
+            this.button100.UseVisualStyleBackColor = true;
+            // 
+            // button50
+            // 
+            this.button50.Location = new System.Drawing.Point(406, 43);
+            this.button50.Name = "button50";
+            this.button50.Size = new System.Drawing.Size(164, 90);
+            this.button50.TabIndex = 10;
+            this.button50.Text = "50.000";
+            this.button50.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(212, 43);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(164, 90);
+            this.button20.TabIndex = 9;
+            this.button20.Text = "20.000";
+            this.button20.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(18, 43);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(164, 90);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "10.000";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1000;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button500;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button200;
+        private System.Windows.Forms.Button button50;
+        private System.Windows.Forms.Button button150;
+        private System.Windows.Forms.Button button100;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Saldo/Saldo/Form1.cs
@@ -0,0 +1,123 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Saldo
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection mySqlConnection;
+        private MySqlCommand mySqlCommand;
+        private MySqlDataAdapter mySqlDataAdapter;
+        private string YourConnectionString = "server=localhost;uid=root;pwd=titi020504;database=latian";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            string customerName = label2.Text;
+            decimal emoneyBalance = GetEmoneyBalance(customerName);
+            label3.Text = emoneyBalance.ToString();
+            button10.Click += MoneyButton_Click;
+            button20.Click += MoneyButton_Click;
+            button50.Click += MoneyButton_Click;
+            button100.Click += MoneyButton_Click;
+            button150.Click += MoneyButton_Click;
+            button200.Click += MoneyButton_Click;
+            button500.Click += MoneyButton_Click;
+            button1000.Click += MoneyButton_Click;
+
+
+
+        }
+        private void MoneyButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            // Show a confirmation dialog
+            DialogResult result = MessageBox.Show("Do you want to top up " + buttonText + "?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Extract the nominal value from the button text
+                string nominal = buttonText.Replace(".", "").Replace(",", "").Replace("Rp", "");
+                decimal topUpAmount = decimal.Parse(nominal);
+
+                // Update the e-money balance in the database
+                string customerName = label2.Text;
+                UpdateEmoneyBalance(customerName, topUpAmount);
+
+                // Refresh the e-money balance display
+                decimal emoneyBalance = GetEmoneyBalance(customerName);
+                label3.Text = emoneyBalance.ToString();
+
+                // Show a success message
+                MessageBox.Show("Successfully topped up " + buttonText + " to e-money balance.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            panel1.Visible = false;
+        }
+        private void UpdateEmoneyBalance(string customerName, decimal topUpAmount)
+        {
+            using (mySqlConnection = new MySqlConnection(YourConnectionString))
+            {
+                mySqlConnection.Open();
+
+                string updateQuery = "UPDATE Customer SET e_money = e_money + @TopUpAmount WHERE Nama_customer = @CustomerName";
+
+                using (mySqlCommand = new MySqlCommand(updateQuery, mySqlConnection))
+                {
+                    mySqlCommand.Parameters.AddWithValue("@TopUpAmount", topUpAmount);
+                    mySqlCommand.Parameters.AddWithValue("@CustomerName", customerName);
+
+                    mySqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private decimal GetEmoneyBalance(string customerName)
+        {
+            decimal emoneyBalance = 0;
+
+            using (mySqlConnection = new MySqlConnection(YourConnectionString))
+            {
+                mySqlConnection.Open();
+
+                string query = "SELECT e_money FROM Customer WHERE Nama_customer = @CustomerName";
+
+                using (mySqlCommand = new MySqlCommand(query, mySqlConnection))
+                {
+                    mySqlCommand.Parameters.AddWithValue("@CustomerName", customerName);
+
+                    using (MySqlDataReader reader = mySqlCommand.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            emoneyBalance = reader.GetDecimal(0); // Assuming the e_money column is of DECIMAL data type
+                        }
+                    }
+                }
+            }
+
+            return emoneyBalance;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+    }
+}
